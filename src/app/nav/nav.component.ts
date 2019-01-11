b52d378a47b887ac393c74717093590605dbd2b7
Luke Baker <akabaker@gmail.com>
2019-01-11 15:57:05 -0600

    blerg

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HammerGestureConfig } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+    selector: 'app-nav',
+    templateUrl: './nav.component.html',
+    styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+    @ViewChild('navList') navList;
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit(): void {
+    }
 
+    showNavigation() {
+        this.navList.nativeElement.style.display = "block";
+    }
 }
